95e21195087158d19ead9fe7dd065742e2568590
Sophia Freire <105390215+Sophia-Freire@users.noreply.github.com>
2022-09-06 10:34:11 -0300

    Create ConversorFahrenheit.js

---
+++ ConversorFahrenheit.js
@@ -0,0 +1,9 @@
+var celsius
+var temperaturaFinal
+
+
+var celsius = prompt("Informe a temperatura ")
+
+temperaturaFinal = (celsius * 9/5) + 32 
+
+alert("A temperatura em Farehrenheit é: " + temperaturaFinal)
